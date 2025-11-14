7bbe40f60290a9efb737dd07833f588904ec6c58
nazzz1324 <66849482+nazzz1324@users.noreply.github.com>
2025-11-14 22:05:13 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    student_name = os.getenv('STUDENT_NAME', 'Девопсов Олег')
+    return render_template('index.html', student_name=student_name)
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 8081))
+    app.run(host='0.0.0.0', port=port)
